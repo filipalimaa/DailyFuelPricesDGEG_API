900212ad693e12ba6459715b7d71340371b595d0
filipalimaa <filipalima1995@gmail.com>
2025-03-09 21:08:35 +0000

    New endpoint created

--- 4.DailyFuelPricesDGEG_API/src/index.ts
+++ 4.DailyFuelPricesDGEG_API/src/index.ts
@@ -1,15 +1,22 @@
 import express from "express";
+import axios from "axios";
 import dotenv from "dotenv";
 
 dotenv.config();
-
 const app = express();
 const PORT = process.env.PORT || 4000;
 
-app.get("/", (req, res) => {
-  res.json({ boas: "pessoal" });
+const API_URL = "https://precoscombustiveis.dgeg.gov.pt/api/PrecoComb/ListarTopPostos?idsTiposComb=3201&qtdPorPagina=5";
+
+app.get("/fuelPrices", async (req, res) => {
+    try {
+        const response = await axios.get(API_URL);
+        res.json(response.data);
+    } catch (error) {
+        res.status(500).json({ error: "Error retrieving data." });
+    }
 });
 
 app.listen(PORT, () => {
-  console.log(`API a correr em http://localhost:${PORT}`);
+    console.log(`API a correr em http://localhost:${PORT}`);
 });
